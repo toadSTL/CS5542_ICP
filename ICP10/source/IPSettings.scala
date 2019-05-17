789d48a6b9609428778110e94bf78067112cb273
toadSTL <gregory.dylan.brown@gmail.com>
2019-05-17 09:34:55 -0500

    ICP 10

---
+++ ICP10/source/IPSettings.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by pradyumnad on 19/07/15.
+  */
+object IPSettings {
+
+  val PATH = "data/"
+  val INPUT_DIR = PATH + "train2"
+  val TEST_INPUT_DIR = PATH + "test3"
+  val FEATURES_PATH = PATH + "model/features"
+
+  val KMEANS_PATH = PATH + "model/clusters"
+  val HISTOGRAM_PATH = PATH + "model/histograms"
+  val DECISION_TREE_PATH = PATH + "model/tdmodel"
+  val NAIEVE_BAYES_PATH = PATH + "model/nbmodel"
+  val RANDOM_FOREST_PATH = PATH + "model/rfmodel"
+}
